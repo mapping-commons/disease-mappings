b2da00c8307a88230c22e3731ea60b79d69198a1
Harshad Hegde <hegdehb@gmail.com>
2022-02-28 16:37:33 -0600

    new sssom files

--- script/add-ext-mapping.py
+++ script/add-ext-mapping.py
@@ -32,9 +32,10 @@ with open(META, "r") as stream:
 # "Narrower Than": "oboInOwl:hasNarrowSynonym"
 
 
-def get_file_from_dir(ont: str, dir: str) -> str:
-    f = [x for x in listdir(dir) if ont in x and x.endswith(".tsv")]
-    return join(dir, f[0])
+def get_file_from_dir(ont: str, dir: str, ext: str = ".tsv") -> str:
+    # sep = "\t" if ext == ".tsv" else ","
+    f = [x for x in listdir(dir) if ont in x and x.endswith(ext)]
+    return f
 
 
 def main(ont):
@@ -45,69 +46,73 @@ def main(ont):
         "TARGET_TERM",
         "TARGET_TERMINOLOGY",
     ]
-    df = pd.read_csv(
-        get_file_from_dir(ont, RAW_INPUT_DIR),
-        sep="\t",
-        low_memory=False,
-        usecols=columns_needed,
-    )
     prefix = PREFIX_DICT[ont]
-    df["CODE"] = prefix + df["CODE"].astype(str)
-    df["RELATIONSHIP_TO_TARGET"] = df["RELATIONSHIP_TO_TARGET"].apply(
-        lambda x: PREDICATE_DICT.get(x)
-    )
+    ncit_file = get_file_from_dir("ncit", MAPPINGS_DIR)[0]
+    ncit_sssom = read_sssom_table(join(MAPPINGS_DIR, ncit_file))
 
-    df["TARGET_CODE"] = (
-        df["TARGET_TERMINOLOGY"].str.split(" ", expand=True)[0]
-        + ":"
-        + df["TARGET_CODE"]
-    )
-
-    new_df = df.drop(columns=["TARGET_TERMINOLOGY"], axis=1)
-
-    new_df = new_df.rename(
-        columns={
-            "CODE": "subject_id",
-            "RELATIONSHIP_TO_TARGET": "predicate_id",
-            "TARGET_CODE": "object_id",
-            "TARGET_TERM": "subject_label",
-        }
-    )
-    new_df.insert(loc=3, column="match_type", value="Unspecified")
-    new_df = new_df.drop_duplicates()
-    ncit_sssom = read_sssom_table(get_file_from_dir("ncit", MAPPINGS_DIR))
-
-    blank_columns = [
-        x
-        for x in ncit_sssom.df.columns.values.tolist()
-        if x not in new_df.columns.values.tolist()
-    ]
-    for col_name in blank_columns:
-        new_df[col_name] = ""
+    # blank_columns = [
+    #     x
+    #     for x in ncit_sssom.df.columns.values.tolist()
+    #     if x not in new_df.columns.values.tolist()
+    # ]
+    # for col_name in blank_columns:
+    #     new_df[col_name] = ""
 
     ncit_sssom.metadata["mapping_date"] = "2018-07-05"
-
-    new_prefix_map = ncit_sssom.prefix_map
+    new_prefix_map = dict()
+    new_prefix_map["NCI"] = ncit_sssom.prefix_map["NCI"]
     new_prefix_map[
         "ICD10CM"
     ] = "http://apps.who.int/classifications/icd10/browse/2010/en#/"
 
-    ncit_icd10_sssom = sssom.util.MappingSetDataFrame(
-        df=new_df,
-        metadata=ncit_sssom.metadata,
-        prefix_map=new_prefix_map,
-    )
+    # merged_msdf = sssom.util.merge_msdf(
+    #     ncit_sssom, ncit_icd10_sssom, reconcile=False
+    # )
 
-    # sssom.from_sssom_dtaframe()
-    merged_msdf = sssom.util.merge_msdf(
-        ncit_sssom, ncit_icd10_sssom, reconcile=False
-    )
+    # merged_msdf.df = merged_msdf.df.dropna(how="all")
+    # merged_msdf.prefix_map = new_prefix_map
+    # merged_msdf.metadata = ncit_sssom.metadata
 
-    merged_msdf.df = merged_msdf.df.dropna(how="all")
-    merged_msdf.prefix_map = new_prefix_map
-    merged_msdf.metadata = ncit_sssom.metadata
-    with open(join(MAPPINGS_DIR, "ncit-icd10.sssom.tsv"), "w") as file:
-        write_table(merged_msdf, file)
+    list_of_files = get_file_from_dir(ont, TMP_DIR, ".csv")
+
+    for fn in list_of_files:
+        fp = join(TMP_DIR, fn)
+        df = pd.read_csv(
+            fp,
+            low_memory=False,
+            usecols=columns_needed,
+        )
+        df["CODE"] = prefix + df["CODE"].astype(str)
+        df["RELATIONSHIP_TO_TARGET"] = df["RELATIONSHIP_TO_TARGET"].apply(
+            lambda x: PREDICATE_DICT.get(x)
+        )
+
+        df["TARGET_CODE"] = (
+            df["TARGET_TERMINOLOGY"].str.split(" ", expand=True)[0]
+            + ":"
+            + df["TARGET_CODE"]
+        )
+
+        new_df = df.drop(columns=["TARGET_TERMINOLOGY"], axis=1)
+
+        new_df = new_df.rename(
+            columns={
+                "CODE": "subject_id",
+                "RELATIONSHIP_TO_TARGET": "predicate_id",
+                "TARGET_CODE": "object_id",
+                "TARGET_TERM": "subject_label",
+            }
+        )
+        # new_df.insert(loc=3, column="match_type", value="Unspecified")
+        new_df = new_df.drop_duplicates()
+        ncit_icd10_sssom = sssom.util.MappingSetDataFrame(
+            df=new_df,
+            metadata=ncit_sssom.metadata,
+            prefix_map=new_prefix_map,
+        )
+        new_fn = fn.replace(".csv", ".sssom.tsv")
+        with open(join(MAPPINGS_DIR, new_fn), "w") as file:
+            write_table(ncit_icd10_sssom, file)
 
 
 if __name__ == "__main__":
